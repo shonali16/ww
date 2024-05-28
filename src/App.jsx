e9c22cfcbafeee36c209dfb355b973ba78032df1
sonalisachdev <sachdevsonali16@gmail.com>
2024-05-27 20:04:14 -0400

    Second comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+
 import React from "react";
+
 import Navbar from "./components/navbar/Navbar";
 function App() {
 
   return (
-    <div>
+    <div className="app">
       <Navbar/>
   
     </div>
   )
 }
 
-export default App
+export default App  
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import "./Navbar.scss";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="logo">
+          <span className="text">WorkWave</span>
+          <div className="dot">.</div>
+        </div>
+        <div className="links">
+          <span>WorkWave Business</span>
+          <span>Sign in </span>
+          <span>Become a seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
