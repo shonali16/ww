43aa48debf6c597f12d6d9e5ac24a91a33da1dd6
thenameisRushi <154388804+thenameisRushi@users.noreply.github.com>
2024-05-28 18:56:44 -0400

    front ENd done (logIn and Register)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,33 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="container">
+      <div className="bottom">
+          <div className="left">
+            <h2>WorkWave</h2>
+            <span>© WorkWave International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="/img/world.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="/img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="/img/accessibility.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import "./login.scss"
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+      <form>
+        <h1>Sign In</h1>
+        <label htmlFor="">Username</label>
+        <input type="text" name="username"/>
+
+        <label htmlFor="">Password</label>
+        <input type="password" name="password"/>
+
+        <button type="submit">Login</button>
+      </form>
+
+    </div>
   )
 }
 
